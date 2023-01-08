d775744f939360198e5b0e33dce263d9b067b848
Anus-Ali-Khan <anusalikhan1405@gmail.com>
2023-01-08 15:59:39 +0500

    feat: Home page completed

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,6 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
+// Colors
 const primaryColor = Color(0xFF183661);
 const secondaryColor = Color(0xFFDD6B4D);
+const white = Colors.white;
